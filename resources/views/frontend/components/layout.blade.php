f6e9406733b116274d98de3be670b048950e77e8
Hairul Bahri <80257426+HairulBahri11@users.noreply.github.com>
2026-02-14 13:27:35 +0700

    test bar pwa

--- resources/views/frontend/components/layout.blade.php
+++ resources/views/frontend/components/layout.blade.php
@@ -36,7 +36,12 @@
 </style>
 
 <link rel="manifest" href="/manifest.json">
-<meta name="theme-color" content="#B4252A">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="application-name" content="Kencana">
+<meta name="apple-mobile-web-app-title" content="Kencana">
+<meta name="theme-color" content="#ffff"> <meta name="msapplication-navbutton-color" content="#ffff">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <script type="text/javascript">
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/serviceworker.js').then(function (registration) {
